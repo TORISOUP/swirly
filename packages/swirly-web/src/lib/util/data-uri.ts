b3878dc3a3068e968b74ccd7c40223c581a7054d
TORISOUP <tori.birdstrike@gmail.com>
2026-04-20 20:26:54 +0900

    fix: encode data URIs as UTF-8

--- packages/swirly-web/src/lib/util/data-uri.ts
+++ packages/swirly-web/src/lib/util/data-uri.ts
@@ -2,4 +2,11 @@ export const buildDataUri = (
   contentType: string,
   charset: string,
   data: string
-) => `data:${contentType};charset=${charset};base64,${encodeURI(btoa(data))}`
+) => {
+  const bytes = new TextEncoder().encode(data)
+  let binary = ''
+  for (const byte of bytes) {
+    binary += String.fromCharCode(byte)
+  }
+  return `data:${contentType};charset=${charset};base64,${btoa(binary)}`
+}
